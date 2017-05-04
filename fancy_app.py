0f4f8f11b39f13b2dc81cda895f37b5bf9180321
corinnecm <corinne.carlson5@gmail.com>
2017-05-04 10:29:38 -0700

    last minute changes to fancy app and templates

--- fancy_app.py
+++ fancy_app.py
@@ -30,28 +30,28 @@ def modeling():
  return render_template('model_template.html')
 
 
-@app.route('/score', methods=['GET'])
-def score():
-    data_path = (requests.get('http://galvanize-case-study-on-fraud.herokuapp.com/data_point').json())
-    record = predict(model, data_path)
-    print record
-    return jsonify(record)
-
-@app.route('/dashboard')
-def dashboard():
-    results = collection.find().sort("timestamp", pymongo.DESCENDING).limit(1)[0]
-    pred = results['prediction']
-    return render_template("index.html", prediction=pred, text=results)
-    return str(results[0])
-
-
-if __name__ == '__main__':
-    data_path = "data/subset.json"
-    pickle_path = "model_files/model_GradientBoostingClassifier.pkl"
-    collection = setup_db()
-    with open(pickle_path) as f:
-        model = pickle.load(f)
-    app.run(host='0.0.0.0', port=8080, debug=True)
+# @app.route('/score', methods=['GET'])
+# def score():
+#     data_path = (requests.get('http://galvanize-case-study-on-fraud.herokuapp.com/data_point').json())
+#     record = predict(model, data_path)
+#     print record
+#     return jsonify(record)
+#
+# @app.route('/dashboard')
+# def dashboard():
+#     results = collection.find().sort("timestamp", pymongo.DESCENDING).limit(1)[0]
+#     pred = results['prediction']
+#     return render_template("index.html", prediction=pred, text=results)
+#     return str(results[0])
+#
+#
+# if __name__ == '__main__':
+#     data_path = "data/subset.json"
+#     pickle_path = "model_files/model_GradientBoostingClassifier.pkl"
+#     collection = setup_db()
+#     with open(pickle_path) as f:
+#         model = pickle.load(f)
+#     app.run(host='0.0.0.0', port=8080, debug=True)
 
 
 if __name__ == '__main__':
